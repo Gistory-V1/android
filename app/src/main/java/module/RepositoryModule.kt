82c5c7e00b25405a6f0b02cb1cde156506224072
kimjeakwan <s24042@gsm.hs.kr>
2024-10-25 09:17:08 +0900

    📝 ::add RepositoryModule

---
+++ app/src/main/java/module/RepositoryModule.kt
@@ -0,0 +1,9 @@
+package com.kim.Dormitorymanager.module
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+class RepositoryModule {
+}
