fa586a344164b3af6e403c165421a73141b73fbd
kimjeakwan <s24042@gsm.hs.kr>
2024-10-27 00:33:34 +0900

    :create: :: Authrepoistory

---
+++ domain/src/main/java/reopoistory/Authrepoistory.kt
@@ -0,0 +1,13 @@
+package reopoistory
+
+import Model.auth.request.GauthloginRequestBodyModel
+import Model.auth.response.GauthloginresponseModel
+import kotlinx.coroutines.flow.Flow
+
+interface Authrepoistory {
+
+    suspend fun GAuthLogin(body: GauthloginRequestBodyModel): Flow<GauthloginresponseModel>
+    suspend fun GAuthAccess(refreshToken: String): Flow<GauthloginresponseModel>
+    suspend fun GauthLogout(): Flow<Unit>
+
+}
