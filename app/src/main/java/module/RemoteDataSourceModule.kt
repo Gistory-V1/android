03bd1d38a666d6e0f0da0989b57b4381790d13f6
kimjeakwan <s24042@gsm.hs.kr>
2024-10-27 00:31:19 +0900

    :create: :: RemoteDataSourceModule

--- app/src/main/java/module/RemoteDataSourceModule.kt
+++ app/src/main/java/module/RemoteDataSourceModule.kt
@@ -4,15 +4,14 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import remote.datasource.Gauthdatasourceimpi
-/*
+import remote.datasource.Gauthdatasourceimpl
+
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RemoteDataSourceModule {
     @Binds
     abstract fun providAuthDatasource(
-    //구현할 datasource
-        gauthdatasourceimpi: Gauthdatasourceimpi
-    ): Gauthdatasourceimpi
+
+        gauthdatasourceimpi: Gauthdatasourceimpl
+    ): Gauthdatasourceimpl
 }
- */
