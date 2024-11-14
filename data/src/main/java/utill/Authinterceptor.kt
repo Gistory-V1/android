f130f474d0bee48da5c735ee7f2a1e302ceb3d61
kimjeakwan <s24042@gsm.hs.kr>
2024-11-14 23:22:22 +0900

    :sparkles: :: Authinterceptor

---
+++ data/src/main/java/utill/Authinterceptor.kt
@@ -0,0 +1,71 @@
+package utill
+
+import com.squareup.moshi.Moshi
+import expection.NeedLoginException
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import local.datasource.LocalDataSource
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody
+import okhttp3.Response
+import javax.inject.Inject
+
+class Authinterceptor @Inject constructor(
+    private val dataSource: LocalDataSource
+) : Interceptor {
+    private val ignore by lazy {"/auth"}
+
+    private companion object {
+        const val POST = "POST"
+        const val GET = "GET"
+        const val DELETE = "DELETE"
+        const val PATCH = "PATCH"
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val path = request.url.encodedPath
+        val method = request.method
+
+        val accessToken = runBlocking { dataSource.getAccessToken().first().replace("\"","") }
+        val refreshTokon = runBlocking { dataSource.getRefreshToken().first().replace("\"","") }
+
+        val newRequest = when{
+            ignore.any { path.contains(it) && method in listOf(POST, GET, DELETE)} -> {
+                request
+            }
+
+            path.endsWith("/auth") && method in listOf(PATCH)-> {
+                request.newBuilder().addHeader("Authorization", "Bearer $refreshTokon").build()
+            }
+            else -> {
+                request.newBuilder().addHeader("Authorization","Bearer $accessToken" ).build()
+            }
+
+
+
+        }
+        val response = chain.proceed(newRequest)
+
+        return when(response.code){
+            201, 204-> response.newBuilder().code(200).build()
+            else -> response
+        }
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
