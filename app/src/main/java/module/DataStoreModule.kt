11aff9df4b3398f708f8b4ae5a14984af99bcd6e
kimjeakwan <s24042@gsm.hs.kr>
2024-11-15 08:21:26 +0900

    :art: :: 공백삭제

--- app/src/main/java/module/DataStoreModule.kt
+++ app/src/main/java/module/DataStoreModule.kt
@@ -14,7 +14,6 @@ import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
-
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
@@ -27,6 +26,4 @@ object DataStoreModule {
             ),
             produceFile = { context.preferencesDataStoreFile("autoDataStore") }
         )
-
-
 }
