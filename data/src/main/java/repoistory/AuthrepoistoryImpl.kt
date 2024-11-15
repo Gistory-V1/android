eb062a0ba6cc9ecbc9184c182631abb4a8098b44
kimjeakwan <s24042@gsm.hs.kr>
2024-11-15 10:55:52 +0900

    :recycle: :: AuthrepoistoryImpl logout suspend 함수 추가

--- data/src/main/java/repoistory/AuthrepoistoryImpl.kt
+++ data/src/main/java/repoistory/AuthrepoistoryImpl.kt
@@ -13,7 +13,7 @@ import javax.inject.Inject
 class AuthrepoistoryImpl @Inject constructor(
     private val gauthdatasource: Gauthdatasource
 ) : Authrepoistory {
-    override fun GauthLogout(): Flow<Unit> {
+    override suspend fun GauthLogout(): Flow<Unit> {
        return gauthdatasource.gauthLogout()
     }
 
