3205835a902bd85285c836dff9352c13e85c9c72
kimjeakwan <s24042@gsm.hs.kr>
2024-11-16 14:52:57 +0900

    :truck: :: GAuthLoginRequest, GAuthLoginResponse

--- data/src/main/java/remote/dto/auth/response/GAuthLoginResponse.kt
+++ data/src/main/java/remote/dto/auth/response/GAuthLoginResponse.kt
@@ -1,17 +1,17 @@
 package remote.dto.auth.response
 
-import Model.auth.response.GauthloginresponseModel
+import Model.auth.response.GAuthLoginResponseModel
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class Gauthloginresponse (
+data class GAuthLoginResponse (
     @Json(name = "accessToken") val accessToken: String,
     @Json(name = "refreshToken") val refreshToken: String,
     @Json(name = "accessTokenExpiresIn") val accessTokenExpiresIn: String,
     @Json(name = "refreshTokenExpiresIn") val refreshTokenExpiresIn: String,
 )
-fun Gauthloginresponse.toLogin()= GauthloginresponseModel(
+fun GAuthLoginResponse.toLogin()= GAuthLoginResponseModel(
     accessToken=accessToken,
     refreshToken=refreshToken,
     accessTokenExpiresIn=accessTokenExpiresIn,
--- domain/src/main/java/Model/auth/request/GAuthLoginRequestBodyModel.kt
+++ domain/src/main/java/Model/auth/request/GAuthLoginRequestBodyModel.kt
@@ -1,5 +1,5 @@
 package Model.auth.request
 
-data class GauthloginRequestBodyModel(
+data class GAuthLoginRequestBodyModel(
     val code: String
 )
