12072576a482db4f55b486ea52b86fabab71b851
kimjeakwan <s24042@gsm.hs.kr>
2024-11-16 16:06:42 +0900

    :recycle: :: AuthRepositoryImpl

--- data/src/main/java/repoistory/AuthRepositoryImpl.kt
+++ data/src/main/java/repoistory/AuthRepositoryImpl.kt
@@ -4,6 +4,7 @@ import Model.auth.request.GAuthLoginRequestBodyModel
 import Model.auth.response.GAuthLoginResponseModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
+import local.datasource.LocalDataSource
 import remote.datasource.GAuthDataSource
 import remote.dto.auth.request.toDto
 import remote.dto.auth.response.toLogin
@@ -11,17 +12,37 @@ import reopoistory.AuthRepository
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
-    private val gauthDatasource: GAuthDataSource
+    private val gAuthDatasource: GAuthDataSource,
+    private val localDataSource: LocalDataSource
+
 ) : AuthRepository {
     override fun gAuthLogout(): Flow<Unit> {
-       return gauthDatasource.gAuthLogout()
+       return gAuthDatasource.gAuthLogout()
     }
 
     override suspend fun gAuthAccess(): Flow<GAuthLoginResponseModel> {
-      return gauthDatasource.gAuthAccess().map { it.toLogin()  }
+      return gAuthDatasource.gAuthAccess().map { it.toLogin()  }
     }
 
     override  fun gAuthLogin(body: GAuthLoginRequestBodyModel): Flow<GAuthLoginResponseModel> {
-        return gauthDatasource.gAuthLogin(body = body.toDto()).map { it.toLogin() }
+        return gAuthDatasource.gAuthLogin(body = body.toDto()).map { it.toLogin() }
+    }
+
+    override fun getRefreshToken(): Flow<String> {
+      return localDataSource.getRefreshToken()
+    }
+
+    override suspend fun saveToken(data: GAuthLoginResponseModel) {
+        localDataSource.setAccessToken(data.accessToken)
+        localDataSource.setRefreshToken(data.refreshToken)
+        localDataSource.setAccessTime(data.accessTokenExpiresIn)
+        localDataSource.setRefreshTime(data.refreshTokenExpiresIn)
+    }
+
+    override suspend fun deleteToken() {
+        localDataSource.deleteAccessToken()
+        localDataSource.deleteRefreshToken()
+        localDataSource.deleteAccessTime()
+        localDataSource.deleteRefreshTime()
     }
 }
