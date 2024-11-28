50d21c8ab624241ce6590ce2e0338c9acc19e323
kimjeakwan <s24042@gsm.hs.kr>
2024-11-28 23:29:20 +0900

    :recycle: Responsive UI

--- presentation/src/main/java/view/main/component/Rankingcomponent.kt
+++ presentation/src/main/java/view/main/component/Rankingcomponent.kt
@@ -1,10 +1,13 @@
 package view.main.component
 
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,80 +18,79 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-data class RankingcomponentltemData(
-    val Rank: String,
-    val name: String,
-    val number: String
-)
+import model.rank.response.RankResponseModel
+import java.util.UUID
+
+
 @Composable
-fun Rankingcomponent (
-    Rank: String,
-    name: String,
-    number: String,
-   modifier: Modifier=Modifier
-
-
-){
-    Row(
-        Modifier
-            .width(300.dp)
-            .height(48.dp)
-            .border(
-                width = 1.dp,
-                color = Color(0xFF4C4C4C),
-                shape = RoundedCornerShape(size = 10.dp)
-            ),
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-    ){
-        Text(
-            text = Rank,
-            style = TextStyle(
-                fontSize = 16.sp,
-
-                fontWeight = FontWeight(700),
-                color = Color(0xFFFFFFFF),
-
+fun RankingComponent(
+    modifier: Modifier = Modifier,
+    data: RankResponseModel,
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = Color(0xFF252525), shape = RoundedCornerShape(size = 10.dp)),
+        verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(57.dp)
+                .border(
+                    width = 1.dp,
+                    color = Color(0xFF4C4C4C),
+                    shape = RoundedCornerShape(size = 10.dp)
+                ),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = data.rank.toString(),
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight(700),
+                    color = Color(0xFFFFFFFF),
+                    )
+            )
+            Spacer(modifier = Modifier.width(20.dp))
+            Text(
+                text = data.name,
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFFFFFFFF),
+                    )
+            )
+            Spacer(modifier = Modifier.width(152.dp))
+            Text(
+                text = data.penaltyPoint.toString(),
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFFFFFFFF),
                 )
-        )
-        Spacer(modifier = Modifier.width(20.dp))
-        Text(
-            text = name,
-            style = TextStyle(
-                fontSize = 16.sp,
-
-                fontWeight = FontWeight(600),
-                color = Color(0xFFFFFFFF),
-
-                )
-        )
-        Spacer(modifier = Modifier.width(152.dp))
-        Text(
-            text = number,
-            style = TextStyle(
-                fontSize = 14.sp,
-                fontWeight = FontWeight(600),
-                color = Color(0xFFFFFFFF),
-
-                )
-        )
-
+            )
+        }
     }
-
-
 }
+
+
 @Composable
 @Preview
-fun PreviewRankingcomponent(){
-    Rankingcomponent(
+fun PreviewRankingComponent() {
+    RankingComponent(
+        data = RankResponseModel(
+            rank = 3,
+            name = "김재관",
+            penaltyPoint = 5,
+            userId = UUID.randomUUID()
+        )
 
-        Rank = "5위",
-        name = "김재관",
-        number = "7회"
 
     )
 }
