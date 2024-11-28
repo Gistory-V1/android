df574ab36bbd5cf3f8f00077a6339cdb96c234d6
kimjeakwan <s24042@gsm.hs.kr>
2024-11-28 23:29:59 +0900

    :recycle: Responsive UI

--- presentation/src/main/java/view/login/screen/LoginScreen.kt
+++ presentation/src/main/java/view/login/screen/LoginScreen.kt
@@ -5,16 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
-import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,19 +20,23 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import view.login.component.DomaGathbutton
+import view.login.component.DoMaGAthButton
 
+@Composable
+internal fun LoginRoute(
+    navigateToHome: () -> Unit
+
+) {
+    //구현할 예정입니다.
+}
 
 @Composable
 fun LoginScreen(
     modifier: Modifier = Modifier,
-
+    ClickButton: () -> Unit,
     navigateToHome: () -> Unit
 
-
-) {
-
-
+    ) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -47,24 +45,19 @@ fun LoginScreen(
             .background(color = Color(0xFF1E1E1E))
     ) {
         Column(
-            Modifier
-                .width(320.dp)
-                .height(200.dp),
+            modifier = Modifier
+                .fillMaxWidth(),
             verticalArrangement = Arrangement.spacedBy(32.dp, Alignment.Top),
             horizontalAlignment = Alignment.Start
         ) {
             Column(
-
-                Modifier
-                    .width(187.dp)
-                    .height(70.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(40.dp),
                 horizontalAlignment = Alignment.Start
-
-
             ) {
                 Text(
-                    text = "안녕하세요!",
-                    style = TextStyle(
+                    text = "안녕하세요!", style = TextStyle(
                         fontSize = 22.sp,
                         fontWeight = FontWeight(700),
                         color = Color(0xFFFFFFFF),
@@ -74,12 +67,9 @@ fun LoginScreen(
                 Text(
                     text = AnnotatedString.Builder("로그인을 해볼까요?").apply {
                         addStyle(
-                            style = SpanStyle(color = Color(0xFF9BFFA6)),
-                            start = 0,
-                            end = 3
+                            style = SpanStyle(color = Color(0xFF9BFFA6)), start = 0, end = 3
                         )
-                    }.toAnnotatedString(),
-                    style = TextStyle(
+                    }.toAnnotatedString(), style = TextStyle(
                         fontSize = 22.sp,
                         fontWeight = FontWeight(900),
                         color = Color(0xFFFFFFFF),
@@ -87,27 +77,24 @@ fun LoginScreen(
                     )
                 )
 
-            }
-
-
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                DomaGathbutton(onClick = navigateToHome)
-
-
-
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(
+                        10.dp,
+                        Alignment.CenterHorizontally
+                    ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    DoMaGAthButton(onClick = ClickButton)
+                }
             }
         }
     }
 }
 
+
 @Preview
 @Composable
 fun PreviewLoginScreen() {
-    LoginScreen(
-        navigateToHome = {}
-    )
+    LoginScreen(ClickButton = {}, navigateToHome = {})
 }
 
