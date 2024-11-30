8498d16e203900ab78c6d93bedcda95ce4810f82
kimjeakwan <s24042@gsm.hs.kr>
2024-11-30 21:58:46 +0900

    :recycle: :: add ","

--- presentation/src/main/java/view/main/component/MyPageButton.kt
+++ presentation/src/main/java/view/main/component/MyPageButton.kt
@@ -24,7 +24,7 @@ import com.kim.presentation.R
 @Composable
 fun MyPageButton(
     modifier: Modifier = Modifier,
-    onClick: () -> Unit
+    onClick: () -> Unit,
 ) {
     Box(
         modifier = modifier
