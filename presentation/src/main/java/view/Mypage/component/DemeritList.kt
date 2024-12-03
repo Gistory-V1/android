d8e186b14c6495fea0973f411cba0670f5cae54f
kimjeakwan <s24042@gsm.hs.kr>
2024-12-03 18:22:04 +0900

    :recycle: :: DemeritList hard coding change

--- presentation/src/main/java/view/Mypage/component/DemeritList.kt
+++ presentation/src/main/java/view/Mypage/component/DemeritList.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kim.presentation.R
-import model.demerit.response.DemeritListResponseModel
 
 
 @Composable
